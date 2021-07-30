79121a76c6f62e8bb349a55841550c1a6406ac7e
Black1978 <chernyi.andrei@gmail.com>
2021-07-30 09:21:32 +0200

    A registration page is added

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,16 +1,24 @@
 import React from 'react'
 import { Button, Card, Container, Form, Row } from 'react-bootstrap'
-import { NavLink } from 'react-router-dom'
-import { REGISTRATION_ROUTE } from '../utils/consts'
+import { NavLink, useLocation } from 'react-router-dom'
+import { LOGIN_ROUTE, REGISTRATION_ROUTE } from '../utils/consts'
 
 const Auth = () => {
+    const location = useLocation()
+    console.log(location)
+    const isLogin = location.pathname === LOGIN_ROUTE
+    console.log(isLogin)
     return (
         <Container
             className='d-flex justify-content-center align-items-center'
             style={{ height: window.innerHeight - 54 }}
         >
             <Card style={{ width: 600 }} className='p-5'>
-                <h2 className='m-auto'>Authorization</h2>
+                {isLogin ? (
+                    <h2 className='m-auto'>Authorization</h2>
+                ) : (
+                    <h2 className='m-auto'>Registration</h2>
+                )}
                 <Form className='d-flex flex-column'>
                     <Form.Control
                         placeholder='Please enter your e-mail'
@@ -21,12 +29,19 @@ const Auth = () => {
                         className='mt-3'
                     ></Form.Control>
                     <Row className='d-flex justify-content-between mt-3 pl-3 pr-3'>
-                        <div>
-                            Don't you have an account yet?
-                            <NavLink to={REGISTRATION_ROUTE}> Registration</NavLink>
-                        </div>
+                        {isLogin ? (
+                            <div>
+                                Don't you have an account yet?
+                                <NavLink to={REGISTRATION_ROUTE}> Registration</NavLink>
+                            </div>
+                        ) : (
+                            <div>
+                                If you have an account
+                                <NavLink to={LOGIN_ROUTE}> Authorization</NavLink>
+                            </div>
+                        )}
                         <Button variant='outline-success' className='mt-3'>
-                            Enter
+                            {isLogin ? 'Enter' : 'Registration'}
                         </Button>
                     </Row>
                 </Form>
